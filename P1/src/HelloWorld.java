4ee4788bcdba04f3592044ff90b3af13ed98c3fb
milololol <milololol@localhost>
2023-07-25 21:56:55 -0300

    Upload inicial

---
+++ P1/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Imprimir "Hello World" no console
+		
+		System.out.println("Hello World! It's my first project using Java.");
+
+	}
+
+}
